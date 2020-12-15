bb9efb4826529110ace75882ee836098e7e1dc8f
Zonotora <Zonotora@hotmail.com>
2020-12-15 20:29:59 +0100

    Fix bug

--- 2020/python/10.py
+++ 2020/python/10.py
@@ -16,8 +16,8 @@ trib = { 1: 1, 2: 1, 3: 2, 4: 4, 5: 7}
 a, b = tee([list(j) for i, j in groupby(part1)])
 next(b, None)
 head, *part2 = list(zip(a,b))
+test1 = list(filter(lambda x: x[0][0] != 3, part2))
 part2 = list(filter(lambda x: x[0][0] == 3, part2))
 part2 = list(map(lambda x: trib.get(len(x[1]) + 1), part2))
-print(trib.get(len(head[0])))
-part2 = reduce(lambda x, y: x*y, part2) * trib.get(len(head[0])) * 2
+part2 = reduce(lambda x, y: x*y, part2) * trib.get(len(head[0]) + 1)
 print(part2)
