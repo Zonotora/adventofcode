0d88e1c7a03fa5ccde9981984c612699690b0060
Zonotora <Zonotora@hotmail.com>
2020-12-15 20:42:20 +0100

    Remove unused code

--- 2020/python/10.py
+++ 2020/python/10.py
@@ -16,7 +16,6 @@ trib = { 1: 1, 2: 1, 3: 2, 4: 4, 5: 7}
 a, b = tee([list(j) for i, j in groupby(part1)])
 next(b, None)
 head, *part2 = list(zip(a,b))
-test1 = list(filter(lambda x: x[0][0] != 3, part2))
 part2 = list(filter(lambda x: x[0][0] == 3, part2))
 part2 = list(map(lambda x: trib.get(len(x[1]) + 1), part2))
 part2 = reduce(lambda x, y: x*y, part2) * trib.get(len(head[0]) + 1)
