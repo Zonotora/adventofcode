9bb31e746f99929f75abf28769816fe2baa0b0f5
Zonotora <Zonotora@hotmail.com>
2021-12-13 07:31:21 +0100

    Update day 13

--- 2021/python/13.py
+++ 2021/python/13.py
@@ -5,7 +5,7 @@ from python import *
 aoc = AdventOfCode("2021", "13", "", newnew)
 
 
-def fold(items):
+def fold(items, p1):
     coords = [
         [int(x) for x in c.strip().split(",")] for c in items[0].split("\n")
     ]
@@ -23,19 +23,20 @@ def fold(items):
                 xx, yy = coords[i]
                 if xx > x:
                     coords[i] = [max(0, x - (xx - x)), yy]
-
+        if p1:
+            break
     return coords
 
 
 @aoc.part(1)
 def part1(items):
-    coords = fold(items)
+    coords = fold(items, True)
     return len(set(map(tuple, coords)))
 
 
 @aoc.part(2)
 def part2(items):
-    coords = fold(items)
+    coords = fold(items, False)
     xmax = max([x for x, y in coords]) + 1
     ymax = max([y for x, y in coords]) + 1
     arr = [["."] * xmax for _ in range(ymax)]
